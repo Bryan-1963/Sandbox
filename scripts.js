e2a1019d92bf828cccccd5814251e1c4692144f8
Bryan-1963 <bryanlstauffer@gmail.com>
2025-08-24 14:32:56 -0700

    ddd

--- scripts.js
+++ scripts.js
@@ -36,7 +36,33 @@
 	ro.observe(document.getElementById("SubMenu"));
 	ro.observe(document.getElementById("SubTitle"));
 	ro.observe(document.getElementById("ContentTitle"));
-
+		
+	//==========================================================================================
+	// ReadJSONFile
+	//==========================================================================================	
+	async function ReadJSONFile(){
+		console.log("Made it to ReadJSONFile, da doo doo doo");
+		var resultsDisplay = document.getElementById("testResults");
+		//var tstContnt = document.getElementById("testContent");
+		let myObject = await fetch("https://bryan-1963.github.io/Sandbox//Test/Test_Files/AnnotatedPhotos_LloydCopeland.json");
+		let myText = await myObject.text();
+		resultsDisplay.innerText = "<br><br>" + myText;
+		console.log("myText = |" + myText + "|");
+		let testObj = JSON.parse(myText);
+		//console.log("testObj = " + JSON.stringify(testObj));
+		//console.log("testObj[0] = " + JSON.stringify(testObj[0]));
+		/*
+		var myHTML = "";
+		myHTML=myHTML + "<figure class='myFigure'>";
+		myHTML=myHTML + "<img src='" + testObj[0]['photoFilePath'];
+		myHTML=myHTML + "' style='max-height: 400px;'>";
+		myHTML=myHTML + "<figcaption>" + testObj[0]['annotation'];
+		myHTML=myHTML + "</figcaption>";
+		myHTML=myHTML + "</figure>";
+		console.log("myHTML=" + myHTML);
+		tstContnt.innerHTML=myHTML;
+		*/
+	}
 	
 	//==========================================================================================
 	// startup
@@ -1739,33 +1765,7 @@
 
 	}	
 
-		
-	//==========================================================================================
-	// ReadJSONFile
-	//==========================================================================================	
-	async function ReadJSONFile(){
-		console.log("Made it to ReadJSONFile, da doo doo doo");
-		var resultsDisplay = document.getElementById("testResults");
-		//var tstContnt = document.getElementById("testContent");
-		let myObject = await fetch("https://bryan-1963.github.io/Sandbox//Test/Test_Files/AnnotatedPhotos_LloydCopeland.json");
-		let myText = await myObject.text();
-		resultsDisplay.innerText = "<br><br>" + myText;
-		console.log("myText = |" + myText + "|");
-		let testObj = JSON.parse(myText);
-		//console.log("testObj = " + JSON.stringify(testObj));
-		//console.log("testObj[0] = " + JSON.stringify(testObj[0]));
-		/*
-		var myHTML = "";
-		myHTML=myHTML + "<figure class='myFigure'>";
-		myHTML=myHTML + "<img src='" + testObj[0]['photoFilePath'];
-		myHTML=myHTML + "' style='max-height: 400px;'>";
-		myHTML=myHTML + "<figcaption>" + testObj[0]['annotation'];
-		myHTML=myHTML + "</figcaption>";
-		myHTML=myHTML + "</figure>";
-		console.log("myHTML=" + myHTML);
-		tstContnt.innerHTML=myHTML;
-		*/
-	}
+
 	
 	
 	//======================================================================================================================================================================================
