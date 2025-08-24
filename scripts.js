0692f48daa53dbbb7e9ba73c51db237a588c1fd9
Bryan-1963 <bryanlstauffer@gmail.com>
2025-08-24 16:05:37 -0700

    V023

--- scripts.js
+++ scripts.js
@@ -39,11 +39,11 @@
 
 	async function readJSONFile(){
 		console.log("Made it to ReadJSONFile, da doo doo doo");
-		var resultsDisplay = document.getElementById("testResults");
-		//var tstContnt = document.getElementById("testContent");
+
+		var tstContnt = document.getElementById("testContent");
 		let myObject = await fetch("https://bryan-1963.github.io/Sandbox/Test/Test_Files/AnnotatedPhotos_LloydCopeland.json");
 		let myText = await myObject.text();
-		resultsDisplay.innerText = "<br><br>" + myText;
+		
 		console.log("myText = |" + myText + "|");
 		let testObj = JSON.parse(myText);
 		console.log("testObj = " + JSON.stringify(testObj));
